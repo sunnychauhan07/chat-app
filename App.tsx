a91e5db7e4b4bd6cdab27226c24bdf1a1d6cf9db
Ibrahim <ikouma269@gmail.com>
2023-01-26 16:10:07 +0800

    project setup

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
 
+SplashScreen.preventAutoHideAsync();
 export default function App() {
+  const [appIsLoading, setAppIsloading] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAppIsloading(true);
+    }, 2000);
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    if (appIsLoading) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsLoading]);
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -13,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
