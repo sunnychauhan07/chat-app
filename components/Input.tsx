8e533d4eb6dd9e7d6b5a74bbfa704418c08e1140
Ibrahim <ikouma269@gmail.com>
2023-03-12 15:38:02 +0800

    input field auth screen

--- components/Input.tsx
+++ components/Input.tsx
@@ -5,15 +5,26 @@ import { FontAwesome } from "@expo/vector-icons";
 
 interface props {
   label: string;
+  IconPack: any;
+  icon: string;
+  errorText: string;
 }
-const Input = ({ label }: props) => {
+const Input = ({ label, IconPack, icon, errorText }: props) => {
   return (
-    <View>
-      <Text>{label}</Text>
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
       <View style={styles.inputContainer}>
-        <FontAwesome name='user-o' size={24} color='black' />
-        <TextInput />
+        {icon && (
+          <IconPack name={icon} size={24} color='black' style={styles.icon} />
+        )}
+        <TextInput style={styles.input} />
       </View>
+
+      {errorText && (
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorText}>{errorText}</Text>
+        </View>
+      )}
     </View>
   );
 };
@@ -22,12 +33,37 @@ const styles = StyleSheet.create({
   container: {
     width: "100%",
   },
+  label: {
+    marginVertical: 8,
+
+    letterSpacing: 0.3,
+  },
   inputContainer: {
     width: "100%",
     backgroundColor: Colors.nearlyWhite,
     borderRadius: 2,
     paddingVertical: 15,
     paddingHorizontal: 10,
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  icon: {
+    marginRight: 10,
+    color: Colors.lightgrey,
+  },
+  input: {
+    color: Colors.blue,
+    flex: 1,
+    letterSpacing: 0.3,
+    paddingTop: 0,
+  },
+  errorContainer: {
+    marginVertical: 5,
+  },
+  errorText: {
+    color: "red",
+    fontSize: 15,
+    letterSpacing: 0.3,
   },
 });
 
--- screens/AuthScreen.tsx
+++ screens/AuthScreen.tsx
@@ -3,12 +3,32 @@ import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import PageContainer from "../components/PageContainer";
 import Input from "../components/Input";
+import { FontAwesome, Feather } from "@expo/vector-icons";
 
 const AuthScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <PageContainer>
-        <Input label='First Name' />
+        <Input
+          label='First Name'
+          IconPack={FontAwesome}
+          icon='user-o'
+          errorText='error'
+        />
+        <Input
+          label='Last Name'
+          IconPack={FontAwesome}
+          icon='user-o'
+          errorText='error'
+        />
+        <Input label='email' IconPack={Feather} icon='mail' errorText='error' />
+
+        <Input
+          label='Password'
+          IconPack={Feather}
+          icon='lock'
+          errorText='error'
+        />
       </PageContainer>
     </SafeAreaView>
   );
